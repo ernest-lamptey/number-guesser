98c7edef47daa038fdf57487f93dce0e84022846
ernest-lamptey <ernest.lamptey@amalitech.org>
2022-02-25 21:03:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Number Guesser
+
+For this project, I just created the functions in the 'script.js' file. All other files were already prepared.
+
+Overall it is satisfying to see how adding a few lines of code can add core functionality to a program.
+
+One of the project requirements was to pop up an alert whenever the user enters a number less than 0 or greather than 9. I was able to do that but the program still run after the alert message so I figured there was no use until later when I'm more confident to effect the requisite changes in the 'game.js' file.
---
+++ script.js
@@ -0,0 +1,28 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const compareGuesses = (userGuess, computerGuess, target) => {
+  if (Math.abs(target - userGuess) <= Math.abs(target - computerGuess)) {
+    return true;
+  } else if (Math.abs(target - userGuess) > Math.abs(target - computerGuess)) {
+    return false;
+  } 
+};
+
+const updateScore = (winner) => {
+  if (winner === 'human') {
+    humanScore += 1;
+  } else if (winner === 'computer') {
+    computerScore += 1;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber += 1;
+}
+
